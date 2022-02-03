8611d1971f306fce687b7b8a626f37ee78af611b
Miheel <mikaelleuf@gmail.com>
2022-02-03 19:05:40 +0100

    roll die, get and set current rolled face, bound pos and orig of die face sprite

--- src/die.cpp
+++ src/die.cpp
@@ -1,17 +1,62 @@
 #include "die.hpp"
 
-Die::Die(std::array<std::string_view, 6> faces) 
-	:faces(faces)
+Die::Die(std::string path, std::array<std::string_view, 6> faces)
+	:faces(faces), rolledDie(spriteFaces[0].first)
 {
 	for (size_t i = 0; i < spriteFaces.size(); i++)
 	{
 		spriteFaces[i] = { sf::Sprite(), sf::Texture() };
-		spriteFaces[i].second.loadFromFile(faces[i].data());
+		spriteFaces[i].second.loadFromFile(path + faces[i].data());
 		spriteFaces[i].first.setTexture(spriteFaces[i].second);
+		spriteFaces[i].first.setScale(0.2f, 0.2f);
 	}
+
+}
+
+void Die::setPosition(float x, float y)
+{
+	for (size_t i = 0; i < spriteFaces.size(); i++)
+	{
+		spriteFaces[i].first.setPosition(x, y);
+	}
+}
+
+void Die::setOrigin(float x, float y)
+{
+	for (size_t i = 0; i < spriteFaces.size(); i++)
+	{
+		spriteFaces[i].first.setOrigin(x, y);
+	}
+}
+
+sf::FloatRect Die::getGlobalBounds()
+{
+	return rolledDie.getGlobalBounds();
+}
+
+sf::FloatRect Die::getLocalBounds()
+{
+	return rolledDie.getLocalBounds();
+}
+
+sf::Sprite& Die::roll()
+{
+	rolledDie = spriteFaces[randGen(0, spriteFaces.size())].first;
+	return rolledDie;
+}
+
+void Die::setFace(std::string face)
+{
+	int index = std::distance(faces.begin(), std::find(faces.begin(), faces.end(), face));
+	rolledDie = spriteFaces.at(index).first;
+}
+
+sf::Sprite& Die::getFace()
+{
+	return rolledDie;
 }
 
 void Die::draw(sf::RenderTarget & target, sf::RenderStates states) const
 {
-	target.draw(spriteFaces[0].first);
+	target.draw(rolledDie);
 }
--- src/die.hpp
+++ src/die.hpp
@@ -8,16 +8,22 @@
 class Die: public sf::Drawable
 {
 public:
-	Die(std::array<std::string_view, 6> faces);
+	Die(std::string path, std::array<std::string_view, 6> faces);
 	~Die() = default;
 
 	void setPosition(float x, float y);
+	void setOrigin(float x, float y);
+	sf::FloatRect getGlobalBounds();
+	sf::FloatRect getLocalBounds();
 	bool contains(float x, float y);
 	bool contains(sf::Event::MouseButtonEvent mouse);
-	sf::Sprite roll();
+	sf::Sprite& roll();
+	void setFace(std::string face);
+	sf::Sprite& getFace();
 
 private:
 	cor::RandGen randGen;
+	sf::Sprite& rolledDie;
 	const std::array<std::string_view, 6> faces;
 
 	std::array<std::pair<sf::Sprite, sf::Texture>, 6> spriteFaces;
