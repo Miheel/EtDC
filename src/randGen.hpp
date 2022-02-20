7dd64fc087f42911764bde1fad0ce6044f6c799b
Miheel <mikaelleuf@gmail.com>
2022-02-20 18:11:39 +0100

    can now generate sequences of non repeating numbers in a range

--- src/randGen.hpp
+++ src/randGen.hpp
@@ -21,13 +21,13 @@ namespace cor {
 
 		/**
 		* @brief parametrisized constructor
-		* @param seed a seed for the random engine 
+		* @param seed a seed for the random engine
 		*/
 		RandGen(std::mt19937::result_type seed)
 			:rng(seed), dist(0, std::numeric_limits<int>::max()) {}
 
 		/**
-		* @brief overloaded function operator 
+		* @brief overloaded function operator
 		* @return a ramdom number between 0 and std::numeric_limits<int>::max()
 		*/
 		uniform_result operator()() {
@@ -43,6 +43,27 @@ namespace cor {
 		*/
 		uniform_result operator()(int range_min, int range_max) {
 			return std::uniform_int_distribution<int>(range_min, range_max)(rng);
+		}
+
+		/**
+		* @brief will generate a sequence of non repeating random numbers with in the spesified range
+		* @param range_min the smalest number in the range
+		* @param range_max the biggest number in the range
+		* @param lnegthOfSequence lenght of the sequence to generate must be smaler than the uppde bound of the range
+		* @return a sequence of non repeating random numbers
+		*/
+		std::unique_ptr<int[]> nonRepeating(int range_min, int range_max, size_t lnegthOfSequence) {
+			std::unique_ptr<int[]> sequence = std::make_unique<int[]>(lnegthOfSequence);
+			uniform_result randNr;
+			for (size_t i = 0; i < lnegthOfSequence; i++)
+			{
+				do
+				{
+					randNr = this->operator()(range_min, range_max);
+				} while (std::find(sequence.get(), sequence.get() + lnegthOfSequence, randNr) != sequence.get() + lnegthOfSequence);
+				sequence[i] = randNr;
+			}
+			return sequence;
 		}
 
 	private:
